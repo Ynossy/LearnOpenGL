35ab1f7f6ae9106ee517a3c30ff98753f6750b7c
Adrian <haadrian@student.ethz.ch>
2022-02-27 19:45:56 +0100

    First Commit

---
+++ build/CMakeFiles/ColorTriangle_example.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ColorTriangle_example.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/Triangle.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triangle.dir/src/empty_window.c.obj"
+  "CMakeFiles/Triangle.dir/src/empty_window.c.obj.d"
+  "CMakeFiles/Triangle.dir/src/gl.c.obj"
+  "CMakeFiles/Triangle.dir/src/gl.c.obj.d"
+  "Triangle.exe"
+  "Triangle.exe.manifest"
+  "Triangle.pdb"
+  "libTriangle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
