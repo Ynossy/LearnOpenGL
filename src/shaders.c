d489f3c9a02829ab41de6ae639e5047735890175
Adrian <haadrian@student.ethz.ch>
2022-03-01 11:48:30 +0100

    Colorful Rectangle

--- src/shaders.c
+++ src/shaders.c
@@ -8,10 +8,10 @@
 #include <stdio.h>
 
 float vertices[] = {
-    0.5f, 0.5f, 0.0f,   // top right
-    0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f, 0.5f, 0.0f   // top left
+    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,  // top left
 };
 unsigned int indices[] = {
     // note that we start from 0!
@@ -22,22 +22,24 @@ unsigned int indices[] = {
 // Vertex Shader
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
-                                 "out vec4 vertexColor;\n" // output to the Fragmet Shader
+                                 "layout (location = 1) in vec3 aColor;\n"
+
+                                 "out vec3 ourColor;\n" // output to the Fragmet Shader
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+                                 "   ourColor = aColor;\n"
                                  "}\0";
 
 // Fragment Shader
 const char *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n" // global variable
+    "in vec3 ourColor;\n" // global variable
     "\n"
     "void main()\n"
     "{\n"
-    "    FragColor = ourColor;\n"
+    "    FragColor = vec4(ourColor, 1.0);\n"
     "}\n";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
@@ -139,8 +141,13 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // position attribute (location = 0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+
+    // position attribute (location = 1), with 3 offset and 6 stride
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
